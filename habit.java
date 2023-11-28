d4bd073275a2d052b8119cd0ec5299e3261cdb4a
alramirez729 <130582806+alramirez729@users.noreply.github.com>
2023-11-28 15:16:21 -0500

    Create habit.java

---
+++ habit.java
@@ -0,0 +1,17 @@
+public class Habit {
+    String habitName;
+    String description;
+    boolean completed = false;
+
+    //above are the qualities that the habit should have so far:
+    // {name of habit, description of habit, whether it's been done}
+
+    public Habit(String habitName, String  description)
+    {
+        this.habitName = habitName;
+        this.description = description;
+    }
+
+
+
+}
